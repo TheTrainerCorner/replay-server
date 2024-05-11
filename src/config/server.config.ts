d09ebbbe2554210fb20fe94c4047a97062952999
Cody Spratford <koreanpanda345@gmail.com>
2024-05-11 13:56:17 -0700

    Update server.config.ts

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs('server', () => ({
-	port: process.env.REPLAY_SERVER_PORT || 3000,
+	port: process.env.REPLAY_SERVER_PORT || 8080,
 	host: process.env.REPLAY_SERVER_HOST || "http://localhost:3000",
 }));
